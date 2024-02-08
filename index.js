8aa235a56febf7426fd0921f04af40b2366b8824
Michael Denness <thisisturboproductions@gmail.com>
2024-02-08 16:00:29 -0500

    First attempt

--- index.js
+++ index.js
@@ -2,73 +2,29 @@ const freelancers = [
   {name: "Alice", price: 30, occupation: "writer"},
   {name: "Bob", price: 50, occupation: "teacher"},
   {name: "Carol", price: 70, occupation: "programmer"}
-]
-
-
-
-
-
-
-const colors = ["red", "green", "blue", "orange", "yellow", "purple"];
-const sizes = ["small", "medium", "large"];
-const maxShapes = 10;
-const shapes = [
-  {
-    color: "red",
-    size: "small",
-  },
-  {
-    color: "yellow",
-    size: "small",
-  },
 ];
 
-// `setInterval` will call `addShape` every 1000 milliseconds (1 second)
-// and return an interval ID that we can use to stop the interval later.
-// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
-const addShapeIntervalId = setInterval(addShape, 1000);
+const addFreelancerInterval = setInterval(addFreelancer, 3000);
 
-render(); // We call this function once to render the initial state
+render();
 
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
- */
 function render() {
-  // Render the squares
-  const squares = document.querySelector("#squares");
-  const squareElements = shapes.map((shape) => {
+  const freelancer = document.querySelector("#freelancers");
+  const freelancerElements = freelancers.map((freelancer) => {
     const element = document.createElement("li");
-    element.classList.add("square", shape.color, shape.size);
+    element.classList.add(freelancer.name, freelancer.price, freelancer.occupation);
     return element;
   });
-  squares.replaceChildren(...squareElements);
-
-  // TODO: Render the circles
-  const circles = document.querySelector("#circles");
-  const circleElements = shapes.map((shape) => {
-    const element = document.createElement("li");
-    element.classList.add("circle", shape.color, shape.size);
-    return element;
-  });
-  circles.replaceChildren(...circleElements);
+  freelancer.replaceChildren(...freelancerElements);
 }
 
-/**
- * Add a random shape to the `shapes` array
- */
-function addShape() {
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  const size = sizes[Math.floor(Math.random() * sizes.length)];
-
-  // TODO: Randomize the size of the shape
-
-  shapes.push({ color, size });
-
-  render();
-
-  // TODO: Stop adding shapes if we've reached the maximum number of shapes
-  if (shapes.length >= maxShapes) {
-    clearInterval(addShapeIntervalId);
-  }
+function addFreelancer() {
+  for (i = 0; i < freelancers.length; i++) {
+    let name = freelancers.name[i];
+    let price = freelancers.price[i];
+    let occupation = freelancers.occupation[i];
+    freelancers.push({name, price, occupation});
+   }
 }
+
+render();
